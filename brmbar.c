3fac0f79d247ee6f8ee5a47b15d03a0b15fa1dc0
Pavol Rusnak <stick@gk2.sk>
2011-04-22 02:26:48 +0200

    reading items wip

--- brmbar.c
+++ brmbar.c
@@ -6,6 +6,8 @@ struct item {
     int price;
 } items[100];
 
+int items_count;
+
 void fill_items()
 {
     char buf[128];
@@ -16,8 +18,33 @@ void fill_items()
     fclose(f);
 }
 
+int read_item() {
+    char buf[128];
+    int i;
+    for (;;) {
+        printf("i> ");
+        if (fgets(buf, 128, stdin)) {
+            for (i = 0; ; ++i) {
+                if (!strncmp( buf, items[0].ean, strlen(items[0].ean) )) {
+                    return i;
+                }
+            }
+            printf("Unknown item %s\n", buf);
+        }
+    }
+}
+
+int read_action() {
+    return 0;
+}
+
 int main()
 {
+    int i, a;
     fill_items();
+    for (;;) {
+        i = read_item();
+        a = read_action();
+    }
     return 0;
 }
