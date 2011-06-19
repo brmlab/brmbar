4dfa0ec3b084731962a49c4c05521fe7bdc5f0fb
Pavol Rusnak <stick@gk2.sk>
2011-06-19 15:05:19 +0200

    work in progress

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,7 +1,27 @@
-DROP TABLE IF EXISTS items;
 CREATE TABLE items (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL,
   price INTEGER NOT NULL,
   code TEXT NOT NULL
+);
+
+CREATE INDEX items_name ON items ( name );
+CREATE INDEX items_price ON items ( price );
+CREATE UNIQUE INDEX items_code ON items ( code );
+
+CREATE TABLE log (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  userid INTEGER NOT NULL,
+  itemid INTEGER,
+  event TEXT,
+  ts INTEGER NOT NULL
+);
+
+CREATE INDEX log_userid ON log ( userid );
+CREATE INDEX log_itemid ON log ( userid );
+CREATE INDEX log_ts ON log ( ts );
+
+CREATE TABLE balance (
+  userid INTEGER PRIMARY KEY,
+  balance INTEGER NOT NULL
 );
