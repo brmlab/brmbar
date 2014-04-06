65741c31f060d0cecf5475c7b8f771043e8f1108
Petr Baudis <pasky@ucw.cz>
2014-04-06 21:29:04 +0200

    brmbar-web: By default hide out-of-stock items

--- brmbar3/brmbar-web.py
+++ brmbar3/brmbar-web.py
@@ -10,8 +10,8 @@ from flask import *
 app = Flask(__name__)
 #app.debug = True
 
-@app.route('/stock')
-def stock():
+@app.route('/stock/')
+def stock(show_all=False):
     # TODO: Use a fancy template.
     # FIXME: XSS protection.
     response = '<table border="1"><tr><th>Id</th><th>Item Name</th><th>Bal.</th></tr>'
@@ -19,12 +19,22 @@ def stock():
         style = ''
         balance = a.balance()
         if balance == 0:
+            if not show_all:
+                continue
             style = 'color: grey; font-style: italic'
         elif balance < 0:
             style = 'color: red'
         response += '<tr style="%s"><td>%d</td><td>%s</td><td>%d</td></tr>' % (style, a.id, a.name, balance)
     response += '</table>'
+    if show_all:
+        response += '<p><a href=".">(hide out-of-stock items)</a></p>'
+    else:
+        response += '<p><a href="all">(show all items)</a></p>'
     return response
+
+@app.route('/stock/all')
+def stockall():
+    return stock(show_all=True)
 
 
 db = Database.Database("dbname=brmbar")
